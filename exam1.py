3a1024afdae3bd8478da0a804c8632fed9afedaf
root <amsharma@redhat.com>
2014-11-13 17:18:53 +0530

    Correct examp1.py

--- exam1.py
+++ exam1.py
@@ -3,22 +3,24 @@
 from sys import argv
 import random
 
-script report = argv
+#script report = argv
 
 def random_number(number):
 	number = random.randint(1, number)
 	return number
 
 def test1():
+	points_earned = 0
 	upperbound = 1000
-	n1 = randome.randint(1, 1000)
-	s1 = "%d is a Even or Odd Number(Type \'even\' | \'odd\'): ?"
+	n1 = random.randint(1, 1000)
+	s1 = "%d is a Even or Odd Number(Type \'even\' | \'odd\'): ?" %n1
+	print s1
 	answer = raw_input("> ")
 	result = even_odd_number(n1)
 	if result == answer:
-		points_earned += 1
-	else
-		points_earned -= 1
+		points_earned = points_earned + 1
+	else:
+		points_earned = points_earned - 1
 		
 	report = s1 + answer 
 	return report, points_earned
@@ -27,7 +29,7 @@ def test1():
 
 
 def even_odd_number(number):
-	if number % 2 == 0
+	if number % 2 == 0:
 		return 'even'
 	else:
 		return 'odd'
@@ -39,8 +41,9 @@ questions regarding mathematics.
 """
 
 check = raw_input("Ready to take: (y/n)")
-if check == 'y'
-	test1
+if check == 'y':
+	output = test1()
+	print output
 else:
 	print "Thanks"
 
